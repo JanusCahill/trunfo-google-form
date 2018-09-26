bd66ed2b81cfba490d2dfc7c3fa2104dc5732994
JanusCahill <balala.lyra@gmail.com>
2018-09-26 15:58:57 -0300

    Projeto atualizado

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_MyAtr = new System.Windows.Forms.Label();
+            this.lbl_MyTitle = new System.Windows.Forms.Label();
+            this.lbl_otherTitle = new System.Windows.Forms.Label();
+            this.lbl_otherAtr = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_reiniciar = new System.Windows.Forms.Button();
+            this.lbl_qntCartasOponente = new System.Windows.Forms.Label();
+            this.lbl_qntCartas = new System.Windows.Forms.Label();
+            this.lbl_Interrogacao = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_MyAtr
+            // 
+            this.lbl_MyAtr.AutoSize = true;
+            this.lbl_MyAtr.Location = new System.Drawing.Point(41, 147);
+            this.lbl_MyAtr.Name = "lbl_MyAtr";
+            this.lbl_MyAtr.Size = new System.Drawing.Size(35, 13);
+            this.lbl_MyAtr.TabIndex = 0;
+            this.lbl_MyAtr.Text = "label1";
+            this.lbl_MyAtr.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_MyTitle
+            // 
+            this.lbl_MyTitle.AutoSize = true;
+            this.lbl_MyTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MyTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_MyTitle.Location = new System.Drawing.Point(6, 35);
+            this.lbl_MyTitle.Name = "lbl_MyTitle";
+            this.lbl_MyTitle.Size = new System.Drawing.Size(54, 16);
+            this.lbl_MyTitle.TabIndex = 1;
+            this.lbl_MyTitle.Text = "hghfhg";
+            // 
+            // lbl_otherTitle
+            // 
+            this.lbl_otherTitle.AutoSize = true;
+            this.lbl_otherTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_otherTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_otherTitle.Location = new System.Drawing.Point(6, 35);
+            this.lbl_otherTitle.Name = "lbl_otherTitle";
+            this.lbl_otherTitle.Size = new System.Drawing.Size(54, 16);
+            this.lbl_otherTitle.TabIndex = 2;
+            this.lbl_otherTitle.Text = "hghfhg";
+            // 
+            // lbl_otherAtr
+            // 
+            this.lbl_otherAtr.AutoSize = true;
+            this.lbl_otherAtr.Location = new System.Drawing.Point(36, 147);
+            this.lbl_otherAtr.Name = "lbl_otherAtr";
+            this.lbl_otherAtr.Size = new System.Drawing.Size(35, 13);
+            this.lbl_otherAtr.TabIndex = 3;
+            this.lbl_otherAtr.Text = "label1";
+            this.lbl_otherAtr.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.lbl_MyTitle);
+            this.groupBox1.Controls.Add(this.lbl_MyAtr);
+            this.groupBox1.Location = new System.Drawing.Point(33, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 274);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sua carta";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.Location = new System.Drawing.Point(21, 221);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(16, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Location = new System.Drawing.Point(20, 198);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(16, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(20, 175);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(16, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(20, 152);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(16, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightCoral;
+            this.groupBox2.Controls.Add(this.lbl_Interrogacao);
+            this.groupBox2.Controls.Add(this.lbl_otherTitle);
+            this.groupBox2.Controls.Add(this.lbl_otherAtr);
+            this.groupBox2.Location = new System.Drawing.Point(396, 34);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(190, 274);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Carta oponente";
+            // 
+            // btn_reiniciar
+            // 
+            this.btn_reiniciar.Location = new System.Drawing.Point(536, 5);
+            this.btn_reiniciar.Name = "btn_reiniciar";
+            this.btn_reiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btn_reiniciar.TabIndex = 8;
+            this.btn_reiniciar.Text = "Reiniciar";
+            this.btn_reiniciar.UseVisualStyleBackColor = true;
+            this.btn_reiniciar.Click += new System.EventHandler(this.btn_reiniciar_Click);
+            // 
+            // lbl_qntCartasOponente
+            // 
+            this.lbl_qntCartasOponente.AutoSize = true;
+            this.lbl_qntCartasOponente.Location = new System.Drawing.Point(432, 18);
+            this.lbl_qntCartasOponente.Name = "lbl_qntCartasOponente";
+            this.lbl_qntCartasOponente.Size = new System.Drawing.Size(35, 13);
+            this.lbl_qntCartasOponente.TabIndex = 10;
+            this.lbl_qntCartasOponente.Text = "label1";
+            // 
+            // lbl_qntCartas
+            // 
+            this.lbl_qntCartas.AutoSize = true;
+            this.lbl_qntCartas.Location = new System.Drawing.Point(74, 18);
+            this.lbl_qntCartas.Name = "lbl_qntCartas";
+            this.lbl_qntCartas.Size = new System.Drawing.Size(35, 13);
+            this.lbl_qntCartas.TabIndex = 5;
+            this.lbl_qntCartas.Text = "label1";
+            this.lbl_qntCartas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Interrogacao
+            // 
+            this.lbl_Interrogacao.AutoSize = true;
+            this.lbl_Interrogacao.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Interrogacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Interrogacao.ForeColor = System.Drawing.Color.Maroon;
+            this.lbl_Interrogacao.Location = new System.Drawing.Point(26, 63);
+            this.lbl_Interrogacao.Name = "lbl_Interrogacao";
+            this.lbl_Interrogacao.Size = new System.Drawing.Size(0, 152);
+            this.lbl_Interrogacao.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 378);
+            this.Controls.Add(this.lbl_qntCartas);
+            this.Controls.Add(this.lbl_qntCartasOponente);
+            this.Controls.Add(this.btn_reiniciar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Trunfo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_MyAtr;
+        private System.Windows.Forms.Label lbl_MyTitle;
+        private System.Windows.Forms.Label lbl_otherTitle;
+        private System.Windows.Forms.Label lbl_otherAtr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btn_reiniciar;
+        private System.Windows.Forms.Label lbl_qntCartasOponente;
+        private System.Windows.Forms.Label lbl_qntCartas;
+        private System.Windows.Forms.Label lbl_Interrogacao;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+/*
+ * Laís Lyra Hatencia, 180471
+ * Ingrid Mariana Inácio Jesus, 
+ * 
+ * 
+ * O que falta fazer: Mostrar feedback sonoro, esconder a carta do oponente depois de certo tempo mostrando
+ * 
+*/
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static int qntCartas = 4;
+        static int qntCartasOponente = 4;
+        Carta[] cartasJogador = new Carta[8];
+        Carta[] cartasOponente = new Carta[8];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////////// FUNÇÃO MAIN /////////////////////////////////////////
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // distribui as oito cartas em dois vetores de quatro aleatoriamente, sem repetir
+            Distribuir();
+
+            Mostrar(cartasJogador, true);
+            Ocultar();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ganhou = 0;
+
+            // vê qual atributo foi escolhido e compara
+            if (radioButton1.Checked)
+            {
+                if (cartasJogador[0].atr.altura > cartasOponente[0].atr.altura)
+                {
+                    ganhou = 1;
+                }
+                else if (cartasJogador[0].atr.altura < cartasOponente[0].atr.altura)
+                {
+                    ganhou = -1;
+                }
+
+            } else if (radioButton2.Checked)
+            {
+                if (cartasJogador[0].atr.comprimento > cartasOponente[0].atr.comprimento)
+                {
+                    ganhou = 1;
+                }
+                else if (cartasJogador[0].atr.comprimento < cartasOponente[0].atr.comprimento)
+                {
+                    ganhou = -1;
+                }
+
+            } else if (radioButton3.Checked)
+            {
+                if (cartasJogador[0].atr.peso > cartasOponente[0].atr.peso)
+                {
+                    ganhou = 1;
+                }
+                else if (cartasJogador[0].atr.peso < cartasOponente[0].atr.peso)
+                {
+                    ganhou = -1;
+                }
+
+            } else if (radioButton4.Checked)
+            {
+                if (cartasJogador[0].atr.idade > cartasOponente[0].atr.idade)
+                {
+                    ganhou = 1;
+                }
+                else if (cartasJogador[0].atr.idade < cartasOponente[0].atr.idade)
+                {
+                    ganhou = -1;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Você precisa selecionar um atributo!");
+            }
+
+            // apenas se um dos botões estiver checado podemos prosseguir
+            if ((radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked) && qntCartas < 8)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+
+                // mostra um pouco a carta do oponente, e depois para
+
+                Mostrar(cartasOponente, false);
+                Refresh();
+                System.Threading.Thread.Sleep(3000);
+
+                // aumenta a quantidade de cartas de quem ganhou, e diminui do outro
+                if (ganhou == 1)
+                {
+                    PassaParaTras(cartasJogador, qntCartas, 0);
+                    qntCartas++;
+                    cartasJogador[qntCartas - 1] = cartasOponente[0];
+                    PassaParaTras(cartasOponente, qntCartasOponente, 0);
+                    qntCartasOponente--;
+                }
+                else if (ganhou == -1)
+                {
+                    PassaParaTras(cartasOponente, qntCartasOponente, 0);
+                    qntCartasOponente++;
+                    cartasOponente[qntCartasOponente - 1] = cartasJogador[0];
+                    PassaParaTras(cartasJogador, qntCartas, 0);
+                    qntCartas--;
+                }
+                else
+                {
+                    PassaParaTras(cartasJogador, qntCartas, 0);
+                    PassaParaTras(cartasOponente, qntCartasOponente, 0);
+                }
+
+
+
+                // verificar se ganhou ou perdeu
+                if (qntCartas == 8)
+                {
+                    MessageBox.Show("Você ganhou!");
+                }
+                else if (qntCartas == 0)
+                {
+                    MessageBox.Show("Você perdeu!");
+                }
+
+                // por fim mostra as cartas do jogador atualizadas
+                Mostrar(cartasJogador, true);
+                Ocultar();
+
+            }
+
+        }
+
+        private void btn_reiniciar_Click(object sender, EventArgs e)
+        {
+            qntCartas = 4;
+            qntCartasOponente = 4;
+
+            Distribuir();
+
+            Mostrar(cartasJogador, true);
+            Ocultar();
+        }
+
+        // cria todas as variáveis que compõe uma carta
+        struct Carta
+        {
+            public string nome;
+            public string num;
+            public Atributos atr;
+        }
+        struct Atributos
+        {
+            public float altura, comprimento;
+            public int peso, idade;
+        }
+
+        // cria as cartas do trunfo
+        private Carta[] CriarCartas()
+        {
+            Carta dino1 = new Carta();
+            dino1.nome = "Herrerassauro";
+            dino1.num = "1A";
+            dino1.atr.altura = 1.5f;
+            dino1.atr.comprimento = 4.5f;
+            dino1.atr.peso = 300;
+            dino1.atr.idade = 231;
+
+            Carta dino2 = new Carta();
+            dino2.nome = "Procompsognato";
+            dino2.num = "1B";
+            dino2.atr.altura = 0.3f;
+            dino2.atr.comprimento = 1.2f;
+            dino2.atr.peso = 1;
+            dino2.atr.idade = 222;
+
+            Carta dino3 = new Carta();
+            dino3.nome = "Patagossauro";
+            dino3.num = "1C";
+            dino3.atr.altura = 8f;
+            dino3.atr.comprimento = 18f;
+            dino3.atr.peso = 16000;
+            dino3.atr.idade = 169;
+
+            Carta dino4 = new Carta();
+            dino4.nome = "Ceratossauro";
+            dino4.num = "1D";
+            dino4.atr.altura = 2f;
+            dino4.atr.comprimento = 6f;
+            dino4.atr.peso = 1000;
+            dino4.atr.idade = 156;
+
+            Carta dino5 = new Carta();
+            dino5.nome = "Pterodáctilo";
+            dino5.num = "2A";
+            dino5.atr.altura = 0.5f;
+            dino5.atr.comprimento = 1f;
+            dino5.atr.peso = 1;
+            dino5.atr.idade = 150;
+
+            Carta dino6 = new Carta();
+            dino6.nome = "Velociraptor";
+            dino6.num = "2B";
+            dino6.atr.altura = 1f;
+            dino6.atr.comprimento = 1.8f;
+            dino6.atr.peso = 15;
+            dino6.atr.idade = 80;
+
+            Carta dino7 = new Carta();
+            dino7.nome = "Triceratopo";
+            dino7.num = "2C";
+            dino7.atr.altura = 6f;
+            dino7.atr.comprimento = 9f;
+            dino7.atr.peso = 6000;
+            dino7.atr.idade = 68;
+
+            Carta dino8 = new Carta();
+            dino8.nome = "Baptornis";
+            dino8.num = "2D";
+            dino8.atr.altura = 0.8f;
+            dino8.atr.comprimento = 1f;
+            dino8.atr.peso = 7;
+            dino8.atr.idade = 83;
+
+            Carta[] v = new Carta[8];
+            v[0] = dino1;
+            v[1] = dino2;
+            v[2] = dino3;
+            v[3] = dino4;
+            v[4] = dino5;
+            v[5] = dino6;
+            v[6] = dino7;
+            v[7] = dino8;
+
+            return v;
+        }
+
+        // mostra a primeira carta do jogador ou do oponente na tela
+        private void Mostrar(Carta[] carta, bool jogador)
+        {
+            if (jogador)
+            {
+                lbl_qntCartas.Text = "Nº de cartas: " + qntCartas;
+                lbl_MyTitle.Text = carta[0].num + " " + carta[0].nome.ToUpper();
+                lbl_MyAtr.Text = "Altura (m): " + carta[0].atr.altura + "\n\nComprimento (m): " + carta[0].atr.comprimento + "\n\nPeso (Kg): " + carta[0].atr.peso + "\n\nViveu há (10^6 anos): " + carta[0].atr.idade + "\n";
+            }
+            else
+            {
+                lbl_Interrogacao.Text = "";
+                lbl_qntCartasOponente.Text = "Nº de cartas: " + qntCartasOponente;
+                lbl_otherTitle.Text = carta[0].num + " " + carta[0].nome.ToUpper();
+                lbl_otherAtr.Text = "Altura (m): " + carta[0].atr.altura + "\n\nComprimento (m): " + carta[0].atr.comprimento + "\n\nPeso (Kg): " + carta[0].atr.peso + "\n\nViveu há (10^6 anos): " + carta[0].atr.idade + "\n";
+            }
+
+        }
+
+        // oculta a carta do oponente
+        private void Ocultar()
+        {
+            lbl_qntCartasOponente.Text = "Nº de cartas: " + qntCartasOponente;
+            lbl_otherTitle.Text = "";
+            lbl_otherAtr.Text = "";
+            lbl_Interrogacao.Text = "?";
+        }
+
+        // embaralha as cartas
+        private void Distribuir()
+        {
+            Carta[] vetor = CriarCartas();
+
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                cartasJogador[i] = vetor[rnd.Next(0, 8)];
+                for (int j = 0; j < i; j++)
+                {
+                    if (cartasJogador[i].num == cartasJogador[j].num)
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                cartasOponente[i] = vetor[rnd.Next(0, 8)];
+                for (int j = 0; j < i; j++)
+                {
+                    if (cartasOponente[i].num == cartasOponente[j].num)
+                    {
+                        i--;
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < 4; j++)
+                {
+                    if (cartasOponente[i].num == cartasJogador[j].num)
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //passa as cartas para trás depois de serem usadas
+        static void PassaParaTras(Carta[] vetor, int quantidade, int indice)
+        {
+            if (indice == quantidade - 1)
+                return;
+
+            Carta aux;
+            aux = vetor[indice];
+            vetor[indice] = vetor[indice + 1];
+            vetor[indice + 1] = aux;
+
+            PassaParaTras(vetor, quantidade, indice + 1);
+        }
+
+        // aumenta ou diminui o numero d cartas
+        static void MudarNumDeCartas(int num)
+        {
+            qntCartas += num;
+            qntCartasOponente -= num;
+        }
+
+    }
+}
